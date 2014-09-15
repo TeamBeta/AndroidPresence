0459eb81731f325443084643ceb1927c68d8e664
Kristoffer Øseth <kristoffer.oseth@gmail.com>
2014-09-15 14:36:46 +0200

    Login + contactsprovider

--- src/com/example/androidpresence/MainActivity.java
+++ src/com/example/androidpresence/MainActivity.java
@@ -1,18 +1,42 @@
 package com.example.androidpresence;
 
-import android.support.v7.app.ActionBarActivity;
+//import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity  {
+	private EditText username = null;
+	private EditText password = null;
+	private Button login;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+	    setContentView(R.layout.activity_main); 
+	    
+	    username = (EditText)findViewById(R.id.editText1);
+	    password = (EditText)findViewById(R.id.editText2);
+	    login = (Button)findViewById(R.id.button1);
 	}
-
+	
+	public void login(View view){
+		if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
+			Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
+			Intent i = new Intent(getApplicationContext(), Contacts.class);
+			i.putExtra("username", username.getText().toString());
+			startActivity(i);
+		}	
+		else{
+			Toast.makeText(getApplicationContext(), "Wrong Password/Username",Toast.LENGTH_SHORT).show();
+		}
+	}	
+	   
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -31,4 +55,20 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+   
+ 
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+ 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+ 
+ 
+	
+
 }
