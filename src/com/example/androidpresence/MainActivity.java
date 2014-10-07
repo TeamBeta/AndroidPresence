afd732af66599a90169c192ff60e4840fbdea131
Simon Hordvik <simon.hordvik@gmail.com>
2014-10-07 11:47:52 +0200

    Initial work on retrieving contact and profile information

--- src/com/example/androidpresence/MainActivity.java
+++ src/com/example/androidpresence/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidpresence;
 
 //import android.support.v7.app.ActionBarActivity;
+import java.util.ArrayList;
+
 import com.example.androidpresence.adapter.TabsPagerAdapter;
 import com.example.androidpresence.sip.EditSIP;
 
@@ -11,9 +13,14 @@ import android.app.ActionBar.Tab;
 import android.app.ActionBar.TabListener;
 import android.app.Activity;
 import android.app.FragmentTransaction;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.Profile;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
@@ -35,11 +42,17 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	private EditText password = null;
 	private Button login;
 	
+	ArrayList<Contact> listOfContacts;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_main); 
 	    
+	    listOfContacts = new ArrayList<Contact>();
+        getContacts(); 
+		getOwnProfileInfo();
+	    	    
 	    viewPager = (ViewPager) findViewById(R.id.pager);
         actionBar = getActionBar();
         mAdapter = new TabsPagerAdapter(getSupportFragmentManager());
@@ -77,10 +90,112 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	    //username = (EditText)findViewById(R.id.editText1);
 	    //password = (EditText)findViewById(R.id.editText2);
 	    //login = (Button)findViewById(R.id.button1);
+        
 	}
 	
+	
+	private void getContacts() {
+		String phoneNumber = null;
+		String email = null;
+		 
+		Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+		String _ID = ContactsContract.Contacts._ID;
+		String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+		String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+		Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+		String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+		String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+		Uri EmailCONTENT_URI =  ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+		String EmailCONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
+		String DATA = ContactsContract.CommonDataKinds.Email.DATA;
+		StringBuffer output = new StringBuffer();
+		ContentResolver contentResolver = getContentResolver();
+		Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null); 
+		
+		Log.d("JADA", "So FAR"); 
+		if (cursor.getCount() > 0) {
+			while (cursor.moveToNext()) { //for every contact
+				ArrayList<String> emails = new ArrayList<String>();
+				String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
+				String name = cursor.getString(cursor.getColumnIndex( DISPLAY_NAME ));				
+				Log.d("contactName", name);
+				int hasPhoneNumber = Integer.parseInt(cursor.getString(cursor.getColumnIndex( HAS_PHONE_NUMBER ))); //get the boolean value of HasPhone and parse to int (0 == false, 1 == true)
+				if (hasPhoneNumber > 0) { //if user has phone number
+					output.append("\n First Name:" + name);
+					// Query and loop for every phone number of the contact
+					Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+					while (phoneCursor.moveToNext()) {
+						phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+						Log.d("CONTACT Phone number",phoneNumber);
+					}
+					phoneCursor.close();
+					// Query and loop for every email of the contact
+					Cursor emailCursor = contentResolver.query(EmailCONTENT_URI, null, EmailCONTACT_ID+ " = ?", new String[] { contact_id }, null);
+					while (emailCursor.moveToNext()) {
+						email = emailCursor.getString(emailCursor.getColumnIndex(DATA));
+						Log.d("CONTACT EMAIL",email);
+						emails.add(email);
+					}
+					emailCursor.close();
+				}
+				Contact contact = new Contact(name,emails);
+				listOfContacts.add(contact);
+			}
+		}	
+	}
+	
+	public void getOwnProfileInfo() {
+		String USER_ID;
+		
+		String[] mProjection = new String[]  //projection are the columns to retrieve => makes the queries more efficient
+			    {
+			        Profile._ID,
+			        Profile.DISPLAY_NAME_PRIMARY,
+			        Profile.LOOKUP_KEY,
+			        Profile.PHOTO_THUMBNAIL_URI,
+			        Profile.CONTACT_PRESENCE
+			    };
+		
+			//THE COLUMNS IN THE PROFILE TABLE
+			String _ID = ContactsContract.Profile._ID;
+			String DISPLAY_NAME = ContactsContract.Profile.DISPLAY_NAME_PRIMARY;
+			String LOOKUP_KEY = ContactsContract.Profile.LOOKUP_KEY;
+			String PHOTO_THUMBNAIL_URI = ContactsContract.Profile.PHOTO_THUMBNAIL_URI;
+			String CONTACT_PRESENCE = ContactsContract.Profile.CONTACT_PRESENCE;
+			
+			//CONTENT URI FOR EMAIL
+			Uri EMAIL_CONTENT_URI = ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+			String EMAIL_CONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
+			String EMAIL_DATA = ContactsContract.CommonDataKinds.Email.DATA;
+			//CONTENT URI FOR SIP
+			String SIP_ADDRESS = ContactsContract.CommonDataKinds.SipAddress.CONTACT_PRESENCE;
+			
+			// Retrieves the profile from the Contacts Provider
+			Cursor mProfileCursor =
+			        getContentResolver().query(
+			                Profile.CONTENT_URI,
+			                mProjection,         
+			                null,
+			                null,
+			                null);
+			if (mProfileCursor.moveToFirst()) // data?
+				Log.d("SO FAR","Sofar");	
+				USER_ID = mProfileCursor.getString(mProfileCursor.getColumnIndex(_ID)); 
+			mProfileCursor.close();
+			Cursor emailCursor = getContentResolver().query(EMAIL_CONTENT_URI, null, EMAIL_CONTACT_ID+ " = ?", new String[] { USER_ID }, null); //translated to SQL => where EmailContact_ID = 'USER_ID';
+			while (emailCursor.moveToNext()) { //for every email that uses has
+				String email = emailCursor.getString(emailCursor.getColumnIndex(EMAIL_DATA)); 
+				Log.d("USER EMAIL",email);
+			}
+			emailCursor.close();  
+	} 
+	
+	
+	
+	
+	
 	@Override
-    public void onTabReselected(Tab tab, FragmentTransaction ft) {
+    public void onTabReselected(Tab tab, FragmentTransaction ft) { 
     }
  
     @Override
