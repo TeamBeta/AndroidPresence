082de6d6a629422926a3109727427e40aab119b9
Simon Hordvik <simon.hordvik@gmail.com>
2014-10-14 13:23:59 +0200

    Simon's contribution

--- src/com/example/androidpresence/Accounts.java
+++ src/com/example/androidpresence/Accounts.java
@@ -5,18 +5,22 @@ import com.example.androidpresence.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
  
 public class Accounts extends Fragment{
  
+	TextView nameLabel;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
- 
         View rootView = inflater.inflate(R.layout.accounts, container, false);
+    	nameLabel = (TextView) rootView.findViewById(R.id.nameLabel);
+    	nameLabel.setText(MainActivity.userName);
         return rootView;
     }
     
